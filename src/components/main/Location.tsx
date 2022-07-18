0c26c3af90d352eb618a4190ca19ff3b0c6f4b43
october-03 <october03@o365sen.net>
2022-07-19 05:24:38 +0900

    Location 수정

--- src/components/main/Location.tsx
+++ src/components/main/Location.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import MainTitleLayout from "../module/MainTitleLayout";
+import map from "../../image/svg/Map.svg";
 
 export default function Location() {
-  return <div>Location</div>;
+  return (
+    <MainTitleLayout title="LOCATION" color="#82969f">
+      <img src={map} alt="" style={{ width: "100%", marginTop: 60 }} />
+    </MainTitleLayout>
+  );
 }
